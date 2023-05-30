e7bab303963b3b05a788cb17b5937aff14182aa1
Adrian Saycon <adrian@mycustomsoftware.com>
2023-05-31 01:48:46 +0800

    Product data manipulation

--- includes/class-wcec-oiw-product.php
+++ includes/class-wcec-oiw-product.php
@@ -20,23 +20,35 @@ class WCEC_OIW_Product
 
     public function __construct()
     {
+        add_action('admin_enqueue_scripts', [$this, 'enqueue_admin_scripts']);
         // add_action('woocommerce_product_after_variable_attributes', [$this, 'wcec_add_custom_field_to_variations'], 10, 3);
         // add_action('woocommerce_save_product_variation', [$this, 'wcec_save_custom_field_variations'], 10, 2);
         // add_filter('woocommerce_available_variation', [$this, 'wcec_add_custom_field_variation_data'], 10, 3);
         add_action('woocommerce_variation_options', [$this, 'add_variation_options'], 10, 3);
     }
-    // add a checkbox after "Enabled" checkbox inside the variations tab
 
+    public function enqueue_admin_scripts($hook)
+    {
+        if ('post.php' == $hook) {
+            global $post;
+            if ($post->post_type == 'shop_order') {
+                wp_enqueue_script('wcec-oiw-admin-product-variations-js', WCECOIW_PLUGIN_URL . 'assets/js/admin_product_variations.js', array('jquery'), '1.2', true);
+                wp_localize_script('wcec-oiw-admin-product-variations-js', 'wcec_ajax', array('ajax_url' => admin_url('admin-ajax.php')));
+            }
+        }
+    }
+
+    // add a checkbox after "Enabled" checkbox inside the variations tab
     public function add_variation_options($loop, $variation_data, $variation)
     {
         woocommerce_wp_checkbox(
             array(
-                'id' => '_mcs_sold_by_weight[' . $variation->ID . ']',
+                'id' => 'wcec_sold_by_weight_option[' . $variation->ID . ']',
                 'wrapper_class' => 'show_if_simple',
                 'label' => __('Sold by Weight', 'woocommerce'),
                 'desc_tip' => 'true',
                 'description' => __('Enable this to add the weight of the product.', 'woocommerce'),
-                'value' => get_post_meta($variation->ID, '_weight', true)
+                'value' => get_post_meta($variation->ID, 'wcec_sold_by_weight_option', true)
             )
         );
     }
@@ -60,15 +72,15 @@ class WCEC_OIW_Product
 
 
     // // Save Checkbox Value
-    // public function wcec_save_custom_field_variations($variation_id, $i)
-    // {
-    //     $checkbox = $_POST['custom_checkbox'][$variation_id];
-    //     if (isset($checkbox)) {
-    //         update_post_meta($variation_id, 'custom_checkbox', 'yes');
-    //     } else {
-    //         update_post_meta($variation_id, 'custom_checkbox', 'no');
-    //     }
-    // }
+    public function wcec_save_custom_field_variations($variation_id, $i)
+    {
+        $checkbox = $_POST['custom_checkbox'][$variation_id];
+        if (isset($checkbox)) {
+            update_post_meta($variation_id, 'custom_checkbox', 'yes');
+        } else {
+            update_post_meta($variation_id, 'custom_checkbox', 'no');
+        }
+    }
 
 
     // Make Checkbox Value available in frontend
--- wcec-oiw.php
+++ wcec-oiw.php
@@ -25,10 +25,110 @@ define('WCECOIW_PLUGIN_URL', plugin_dir_url(__FILE__));
 // Include required files
 require_once WCECOIW_PLUGIN_DIR . 'includes/class-wcec-oiw-product.php';
 require_once WCECOIW_PLUGIN_DIR . 'includes/class-wcec-oiw-order.php';
-// require_once WCECOIW_PLUGIN_DIR . 'includes/class-wcec-oiw-email.php';
-
+require_once WCECOIW_PLUGIN_DIR . 'includes/class-wcec-oiw-email.php';
 
 // Initialize the plugin classes
 WCEC_OIW_Product::init();
 WCEC_OIW_Order::init();
-// WCEC_OIW_Email::init();
+WCEC_OIW_Email::init();
+
+// // Save product variation data
+// add_action('woocommerce_save_product_variation', 'save_variation_weight', 10, 2);
+// function save_variation_weight($variation_id, $i)
+// {
+//     if (isset($_POST['_wcec_sold_by_weight'][$variation_id])) {
+//         update_post_meta($variation_id, '_wcec_sold_by_weight', wc_clean($_POST['_wcec_sold_by_weight'][$variation_id]));
+//     }
+// }
+
+// // Add variation data to the cart item
+// add_filter('woocommerce_add_cart_item_data', 'add_variation_weight_to_cart_item', 10, 3);
+// function add_variation_weight_to_cart_item($cart_item_data, $product_id, $variation_id)
+// {
+//     if (isset($_POST['_wcec_sold_by_weight'][$variation_id])) {
+//         $cart_item_data['wcec_sold_by_weight'] = wc_clean($_POST['_wcec_sold_by_weight'][$variation_id]);
+//     }
+//     return $cart_item_data;
+// }
+
+// // Add variation data to the order item
+// add_action('woocommerce_checkout_create_order_line_item', 'add_variation_weight_to_order_item', 10, 4);
+// function add_variation_weight_to_order_item($item, $cart_item_key, $values, $order)
+// {
+//     if (isset($values['wcec_sold_by_weight'])) {
+//         $item->update_meta_data('wcec_sold_by_weight', $values['wcec_sold_by_weight']);
+//     }
+// }
+
+// // Display variation data in the cart
+// add_filter('woocommerce_cart_item_name', 'display_variation_weight_in_cart', 10, 3);
+// function display_variation_weight_in_cart($product_name, $cart_item, $cart_item_key)
+// {
+//     if (isset($cart_item['wcec_sold_by_weight'])) {
+//         $product_name .= '<br /><div class="mcs-sold-by-weight">' . __('Sold by Weight:', 'wc-editable-calculated-order-item-weights') . ' ' . $cart_item['wcec_sold_by_weight'] . '</div>';
+//     }
+//     return $product_name;
+// }
+
+// // Display variation data in the order
+// add_action('woocommerce_order_item_meta_start', 'display_variation_weight_in_order', 10, 4);
+// function display_variation_weight_in_order($item_id, $item, $order, $plain_text)
+// {
+//     if (isset($item['wcec_sold_by_weight'])) {
+//         echo '<br /><div class="mcs-sold-by-weight"><strong>' . __('Sold by Weight:', 'wc-editable-calculated-order-item-weights') . '</strong> ' . $item['wcec_sold_by_weight'] . '</div>';
+
+//     }
+// }
+
+/* Solution 2 */
+
+// save custom variation option data
+add_action('woocommerce_save_product_variation', 'save_custom_field_variations', 10, 2);
+function save_custom_field_variations($variation_id, $i)
+{
+    $checkbox = $_POST['wcec_sold_by_weight_option'][$variation_id];
+    if (isset($checkbox)) {
+        update_post_meta($variation_id, 'wcec_sold_by_weight_option', 'yes');
+    } else {
+        update_post_meta($variation_id, 'wcec_sold_by_weight_option', 'no');
+    }
+}
+
+// add custom variation option data to the cart item
+add_filter('woocommerce_add_cart_item_data', 'add_custom_field_to_cart_item', 10, 3);
+function add_custom_field_to_cart_item($cart_item_data, $product_id, $variation_id)
+{
+    if (isset($_POST['wcec_sold_by_weight_option'][$variation_id])) {
+        $cart_item_data['wcec_sold_by_weight_option'] = wc_clean($_POST['wcec_sold_by_weight_option'][$variation_id]);
+    }
+    return $cart_item_data;
+}
+
+// add custom variation option data to the order item
+add_action('woocommerce_checkout_create_order_line_item', 'add_custom_field_to_order_item', 10, 4);
+function add_custom_field_to_order_item($item, $cart_item_key, $values, $order)
+{
+    if (isset($values['wcec_sold_by_weight_option'])) {
+        $item->update_meta_data('wcec_sold_by_weight_option', $values['wcec_sold_by_weight_option']);
+    }
+}
+
+// display custom variation option data in the cart
+add_filter('woocommerce_cart_item_name', 'display_custom_field_in_cart', 10, 3);
+function display_custom_field_in_cart($product_name, $cart_item, $cart_item_key)
+{
+    if (isset($cart_item['wcec_sold_by_weight_option'])) {
+        $product_name .= '<br /><div class="mcs-custom-checkbox">' . __('Custom Checkbox:', 'wc-editable-calculated-order-item-weights') . ' ' . $cart_item['wcec_sold_by_weight_option'] . '</div>';
+    }
+    return $product_name;
+}
+
+// display custom variation option data in the order
+add_action('woocommerce_order_item_meta_start', 'display_custom_field_in_order', 10, 4);
+function display_custom_field_in_order($item_id, $item, $order, $plain_text)
+{
+    if (isset($item['wcec_sold_by_weight_option'])) {
+        echo '<br /><div class="mcs-custom-checkbox"><strong>' . __('Custom Checkbox:', 'wc-editable-calculated-order-item-weights') . '</strong> ' . $item['wcec_sold_by_weight_option'] . '</div>';
+
+    }
+}
